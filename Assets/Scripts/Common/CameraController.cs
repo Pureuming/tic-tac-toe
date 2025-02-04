6e6257c24e1070527e1016eec39c98533373d770
Wable <woqja0307@naver.com>
2025-02-04 10:51:21 +0900

    GameManager, BlockController, Block 추가

---
+++ Assets/Scripts/Common/CameraController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float widthUnit = 6f;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+        _camera.orthographicSize = widthUnit / _camera.aspect / 2;
+    }
+}
