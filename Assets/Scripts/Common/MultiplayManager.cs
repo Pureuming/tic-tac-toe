3a837b42c4ac9815455ba293547a0febf739050b
Pureuming <144452024+Pureuming@users.noreply.github.com>
2025-03-07 11:38:28 +0900

    Chatting 예제 구현

---
+++ Assets/Scripts/Common/MultiplayManager.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using SocketIOClient;
+
+// game.js 에서 보내는 값을 받음
+public class RoomData
+{
+    [JsonProperty("roomId")]
+    public string roomId { get; set; }
+}
+
+public class UserData
+{
+    [JsonProperty("userId")]
+    public string userId { get; set; }
+}
+
+public class MessageData
+{
+    [JsonProperty("nickName")]
+    public string nickName { get; set; }
+    [JsonProperty("message")]
+    public string message { get; set; }
+}
+
+public class MultiplayManager
+{
+    private SocketIOUnity _socket;
+    private event Action<Constants.MultiplayManagerState, string> _onMultiplayStateChanged;
+    public Action<MessageData> OnReceivedMessage;
+
+    public MultiplayManager(Action<Constants.MultiplayManagerState, string> onMultiplayStateChanged)
+    {
+        _onMultiplayStateChanged = onMultiplayStateChanged;
+        
+        var uri = new Uri(Constants.GameServerURL);
+        _socket = new SocketIOUnity(uri, new SocketIOOptions
+        {
+            Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
+        });
+        
+        // 서버가 보낼 event(message)에 따라 작동
+        // "createRoom"이라는 event가 왔을 때 CreateRoom이 작동하도록 CreateRoom()이라고 적지 않는다.
+        _socket.On("createRoom", CreateRoom);
+        _socket.On("joinRoom", JoinRoom);
+        _socket.On("startGame", StartGame);
+        _socket.On("gameEnded", GameEnded);
+        _socket.On("receiveMessage", ReceiveMessage);
+        
+        _socket.Connect();
+    }
+
+    private void CreateRoom(SocketIOResponse response)  // 매개변수로 SocketIOResponse가 필수로 있어야 한다.
+    {
+        var data = response.GetValue<RoomData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.CreateRoom, data.roomId);
+    }
+
+    private void JoinRoom(SocketIOResponse response)
+    {
+        var data = response.GetValue<RoomData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.JoinRoom, data.roomId);
+    }
+
+    private void StartGame(SocketIOResponse response)
+    {
+        var data = response.GetValue<UserData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.StartGame, data.userId);
+    }
+
+    private void GameEnded(SocketIOResponse response)
+    {
+        var data = response.GetValue<UserData>();
+        _onMultiplayStateChanged?.Invoke(Constants.MultiplayManagerState.EndGame, data.userId);
+    }
+
+    // 서버로부터 Message를 받음
+    private void ReceiveMessage(SocketIOResponse response)
+    {
+        var data = response.GetValue<MessageData>();
+        OnReceivedMessage?.Invoke(data);
+    }
+
+    // 서버로 Message를 보냄
+    public void SendMessage(string roomId , string nickName, string message)
+    {
+        _socket.Emit("sendMessage", new { roomId, nickName, message });
+    }
+}
--- Assets/Scripts/Test/ChattingPanelController.cs
+++ Assets/Scripts/Test/ChattingPanelController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -9,10 +10,56 @@ public class ChattingPanelController : MonoBehaviour
     [SerializeField] private GameObject messageTextPrefab;
     [SerializeField] private Transform messageTextParent;
 
+    private MultiplayManager _multiplayManager;
+    private string _roomId;
+
     public void OnEndEditInputField(string messageText)
     {
         var messageTextObject = Instantiate(messageTextPrefab, messageTextParent);
         messageTextObject.GetComponent<TMP_Text>().text = messageText;
         messageInputField.text = "";    // message를 보내고 나면 InputField를 비워서 다음 message를 받을 수 있도록
+
+        if (_roomId != null && _multiplayManager != null)
+        {
+            // TODO: 임의로 넣은 "홍길동" 대신 로그인할 때 받아온 User의 nickName을 넣어주기
+            _multiplayManager.SendMessage(_roomId, "홍길동", messageText);
+        }
+    }
+
+    private void Start()
+    {
+        messageInputField.interactable = false;
+        _multiplayManager = new MultiplayManager((state, id) =>
+        {
+            switch (state)
+            {
+                case Constants.MultiplayManagerState.CreateRoom:
+                    Debug.Log("## Create Room ##");
+                    _roomId = id;
+                    break;
+                case Constants.MultiplayManagerState.JoinRoom:
+                    Debug.Log("## Join Room ##");
+                    _roomId = id;
+                    messageInputField.interactable = true;
+                    break;
+                case Constants.MultiplayManagerState.StartGame:
+                    Debug.Log("## Start Game ##");
+                    messageInputField.interactable = true;
+                    break;
+                case Constants.MultiplayManagerState.EndGame:
+                    Debug.Log("## End Game ##");
+                    break;
+            }
+        });
+        _multiplayManager.OnReceivedMessage = OnReceiveMessage;
+    }
+
+    private void OnReceiveMessage(MessageData messageData)
+    {
+        UnityThread.executeInUpdate(() =>
+        {
+            var messageTextObject = Instantiate(messageTextPrefab, messageTextParent);
+            messageTextObject.GetComponent<TMP_Text>().text = messageData.nickName + " : " + messageData.message;
+        });
     }
 }
